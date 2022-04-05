e438d5e8018158d6e923e4d04a4c3b564786fef6
JakariaJishan <jakariajishan987@gmail.com>
2022-04-05 21:30:32 +0600

    heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
 const admin = require("firebase-admin");
